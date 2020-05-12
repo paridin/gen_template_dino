68dced4994e1660e2d576465f7fb5594cd9e0331
Roberto Estrada <restrada@paridin.com>
2020-05-11 22:30:29 -0500

    update to mix format

--- template/$PROJECT_NAME$/.formatter.exs
+++ template/$PROJECT_NAME$/.formatter.exs
@@ -1,5 +1,6 @@
 [
   import_deps: [:ecto_sql, :phoenix],
   inputs: ["mix.exs", "*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  subdirectories: ["priv/*/migrations"]
+  subdirectories: ["priv/*/migrations"],
+  locals_without_parens: [plug: 1, plug: 2]
 ]
