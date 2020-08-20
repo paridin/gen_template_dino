a696d610749a4b185499baf36814235b468c3153
Roberto Estrada <restrada@paridin.com>
2020-08-20 01:23:58 -0500

    0.1.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule GenTemplateDino.Mixfile do
   use Mix.Project
 
   @name :gen_template_dino
-  @version "0.1.0"
+  @version "0.1.1"
 
   @deps [
     {:mix_templates, ">0.0.0", app: false},
