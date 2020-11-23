9098782540fe73d224ea05c2275a2136ab0d4517
Roberto Estrada <restrada@paridin.com>
2020-11-22 21:01:12 -0600

    helper for log in

--- template/$PROJECT_NAME$/test/support/conn_case.ex
+++ template/$PROJECT_NAME$/test/support/conn_case.ex
@@ -12,7 +12,7 @@ defmodule <%= @project_name_camel_case %>Web.ConnCase do
   inside a transaction which is reset at the beginning
   of the test unless the test case is marked as async.
   """
-
+  import Plug.BasicAuth
   use ExUnit.CaseTemplate
 
   using do
@@ -37,5 +37,18 @@ defmodule <%= @project_name_camel_case %>Web.ConnCase do
     end
 
     {:ok, conn: Phoenix.ConnTest.build_conn()}
+  end
+
+  defmodule Helpers do
+    @moduledoc false
+    def log_in_basic(conn) do
+      username = "<%= @basic_auth_user %>"
+      password = "<%= @basic_auth_password %>"
+
+      conn
+      |> Phoenix.ConnTest.init_test_session(%{})
+      |> Plug.Conn.put_req_header("authorization", encode_basic_auth(username, password))
+      |> basic_auth(username: username, password: password)
+    end
   end
 end
