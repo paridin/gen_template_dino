2ff9f5d1400fa21fef795b9c948bd03b700611db
Roberto Estrada <restrada@paridin.com>
2020-11-22 23:15:17 -0600

    add release file

---
+++ template/$PROJECT_NAME$/lib/release.ex
@@ -0,0 +1,28 @@
+
+defmodule <%= @project_name_camel_case %>.Release do
+  @moduledoc false
+  # To run on Docker image replace the last line with the following line.
+  # CMD trap 'exit' INT; /opt/app/bin/$(ls /opt/app/bin) eval "<%= @project_name_camel_case %>.Release.migrate" && /opt/app/bin/$(ls /opt/app/bin) start
+  @app :<%= @project_name %>
+
+  def migrate do
+    load_app()
+
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    end
+  end
+
+  def rollback(repo, version) do
+    load_app()
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  defp repos do
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+
+  defp load_app do
+    Application.load(@app)
+  end
+end
