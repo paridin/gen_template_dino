fa584b063558b3b38b7584fb951a7822a818b28b
Roberto Estrada <restrada@paridin.com>
2021-05-30 23:16:58 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule GenTemplateDino.Mixfile do
   use Mix.Project
 
   @name :gen_template_dino
-  @version "0.1.6"
+  @version "0.1.7"
 
   @deps [
     {:mix_templates, ">0.0.0", app: false},
